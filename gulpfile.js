e76a031350d77abe3a71dd65ff3bbbe8e450b3cf
Fahrettin Aksoy <aksoy@fahrettinaksoy.com.tr>
2019-03-06 21:22:33 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,49 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var mustache = require('gulp-mustache');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var cleancss = require('gulp-clean-css');
 
 gulp.task('server', function(){
 	return connect.server({
 		root: 'dist',
-		port: 3000
+		port: 3000,
+		livereload: true
 	});
 });
-
-gulp.task('mustache', function(){
+gulp.task('style', function(){
+	return gulp.src('./assets/style/*.scss')
+	.pipe( sourcemaps.init() )
+	.pipe( sass().on('error', sass.logError) )
+	.pipe( autoprefixer() )
+	.pipe( gulp.dest('./dist/common/css') )
+	.pipe( cleancss() )
+	.pipe( rename({ suffix: '.min' }) )
+	.pipe( sourcemaps.write('./') )
+	.pipe( gulp.dest('./dist/common/css') );
+});
+gulp.task('javascript', function(){
+	return gulp.src('./assets/javascript/*.js')
+	.pipe( sourcemaps.init() )
+	.pipe( gulp.dest('./dist/common/js') )
+	.pipe( uglify() )
+	.pipe( rename({ suffix: '.min' }) )
+	.pipe( sourcemaps.write('./') )
+	.pipe( gulp.dest('./dist/common/js') );
+});
+gulp.task('html', function(){
 	return gulp.src('./src/*.html')
 	.pipe( mustache() )
 	.pipe( gulp.dest('./dist') );
 });
-
 gulp.task('watch', function(){
-	gulp.watch(['./src/**/*.html', './src/**/*.mustache'], ['mustache']);
+	gulp.watch('./assets/style/*.scss', ['style']);
+	gulp.watch('./assets/javascript/*.js', ['javascript']);
+	gulp.watch(['./src/**/*.html', './src/**/*.mustache'], ['html']);
 });
-
-gulp.task('build', ['mustache']);
-gulp.task('default', ['server', 'mustache', 'watch']);
+gulp.task('build', gulp.series('html', 'javascript', 'style'));
+gulp.task('default', gulp.series('server', 'html', 'javascript', 'style', 'watch'));
